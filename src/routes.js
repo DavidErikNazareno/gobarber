b3abc385a3dcfcf0c66b691fb3c491bb6c21ba67
DavidErikNazareno <daviderikfalsh@yahoo.com.br>
2019-10-10 23:32:10 -0300

    aula avatar do usuario

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -18,8 +19,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
