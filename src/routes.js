cf737dc56c47621b77f0a57f6a6ab61db1ac1595
DavidErikNazareno <daviderikfalsh@yahoo.com.br>
2019-10-15 01:24:28 -0300

    Appointment marcando hora

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
 import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -21,6 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
