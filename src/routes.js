820521c914cb241ed63d65910b95c1717139e688
DavidErikNazareno <daviderikfalsh@yahoo.com.br>
2019-10-21 21:59:44 -0300

    listando agenda no schedule

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
 import ProviderController from './app/controller/ProviderController';
 import AppointmentController from './app/controller/AppointmentController';
+import ScheludeController from './app/controller/ScheludeController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheludeController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
